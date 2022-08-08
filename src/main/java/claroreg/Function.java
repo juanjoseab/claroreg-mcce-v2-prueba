0c09367d288b54b146fb0a0ed6185233b167e72f
Juan Baires <jbaires@sigel.gt>
2022-08-08 17:17:48 -0600

    PRUEB 1

--- src/main/java/claroreg/Function.java
+++ src/main/java/claroreg/Function.java
@@ -1,11 +1,13 @@
 package claroreg;
 
+import com.google.gson.JsonObject;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.HttpResponseMessage;
 import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.BindingName;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 
@@ -20,24 +22,39 @@ public class Function {
      * 1. curl -d "HTTP Body" {your host}/api/HttpExample
      * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
      */
-    @FunctionName("HttpExample")
-    public HttpResponseMessage run(
+    @FunctionName("base_path")
+    public HttpResponseMessage run_base (
             @HttpTrigger(
-                name = "req",
-                methods = {HttpMethod.GET, HttpMethod.POST},
-                authLevel = AuthorizationLevel.ANONYMOUS)
-                HttpRequestMessage<Optional<String>> request,
-            final ExecutionContext context) {
+                    name = "req",
+                    route = "{resource}/{method}/{arg1=EMPTY}/{arg2=EMPTY}/{arg3=EMPTY}",
+                    methods = {
+                            HttpMethod.GET,
+                            HttpMethod.POST,
+                            HttpMethod.PUT,
+                            HttpMethod.DELETE,
+                            HttpMethod.HEAD,
+                            HttpMethod.OPTIONS,
+                            HttpMethod.TRACE,
+                            HttpMethod.CONNECT
+                    },
+                    authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context,
+            @BindingName("resource") String resource,
+            @BindingName("method") String method,
+            @BindingName("arg1") String arg1,
+            @BindingName("arg2") String arg2,
+            @BindingName("arg3") String arg3
+    ){
         context.getLogger().info("Java HTTP trigger processed a request.");
 
-        // Parse query parameter
-        final String query = request.getQueryParameters().get("name");
-        final String name = request.getBody().orElse(query);
+        JsonObject r = new JsonObject();
+        r.addProperty("resource", resource);
+        r.addProperty("method", method);
+        r.addProperty("arg1", arg1);
+        r.addProperty("arg2", arg2);
+        r.addProperty("arg3", arg3);
 
-        if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
-        } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
-        }
+        return request.createResponseBuilder(HttpStatus.OK).body(r).build();
+
     }
 }
